ca34770028fa139847b614562d0aec749bb29549
goliatone <burgosemi@gmail.com>
2015-10-14 21:48:31 -0400

    template config.lua updated with latest attributes

--- src/template.config.lua
+++ src/template.config.lua
@@ -1,3 +1,19 @@
 --- export to global namespace
+
+--- WiFi settings
 SSID = "<SOME_FUNKY_SSID_NAME>"
 PSWD = "<SOME_FUNKY_PASSWORD>"
+
+--- Self registration
+SELF_REGISTRATION = "register.lua"
+REGISTRATION_ENDPOINT = "/pir/movement"
+
+--- Movement sensor sink info
+PORT = 8976
+IP = "192.168.1.146"
+ENDPOINT = "/pir/movement"
+
+---
+PIR = 2
+LED = 0
+DEBUG = true
