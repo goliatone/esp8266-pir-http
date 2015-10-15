03ca264f626b737593cc038b17775be288c505bc
goliatone <burgosemi@gmail.com>
2015-10-14 23:09:51 -0400

    register refactoring

--- src/register.lua
+++ src/register.lua
@@ -1,34 +1,26 @@
 
 local exports = function(ip, port, endpoint)
 
+    --- ego trip, create "self". Self made.
     local self = {}
     self.IP = ip
     self.PORT = port
     self.ENDPOINT = endpoint
 
+    --- construct payload template,
+    --- to be sent during registration
     self.payload = {}
     self.payload.status = "online"
     self.payload.type = "ESP8266"
     self.payload.tag = "PIR"
     self.payload.alias = node.chipid()
 
-    function build_post_request(path, value)
-        payload = cjson.encode(value)
-
-        return "POST " .. path .. " HTTP/1.1\r\n" ..
-        "Host: " .. IP .. "\r\n" ..
-        "Connection: close\r\n" ..
-        "Content-Type: application/json\r\n" ..
-        "Content-Length: " .. string.len(payload) .. "\r\n" ..
-        "\r\n" .. payload
-    end
-
     function self.register(callback)
         conn = net.createConnection(net.TCP, 0)
         conn:connect(self.PORT, self.IP)
 
         --- Create HTTP POST raw headers and body
-        request = build_post_request(self.ENDPOINT, self.payload)
+        request = util.build_post_request(self.IP, self.ENDPOINT, self.payload)
 
         conn:send(request, function()
             print("Registration request sent")
