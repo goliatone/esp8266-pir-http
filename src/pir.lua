a38af8b03a9c0fff9f73936504ab2423653e42b7
goliatone <burgosemi@gmail.com>
2015-10-14 21:47:50 -0400

    pir encapsulated in object

--- src/pir.lua
+++ src/pir.lua
@@ -1,69 +1,72 @@
-PIR = 2
-LED = 0
-DEBUG = true
-IP = "192.168.1.146"
-PORT = 8976
-ENDPOINT = "/pir/movement"
 
---- Self registration 
-SELF_REGISTRATION = "register.lua"
-REGISTRATION_ENDPOINT = "/pir/movement"
+local exports = function()
+    self = {}
+    self.movement = 0
 
-chipId = node.chipid()
-gpio.mode(LED, gpio.OUTPUT)
-gpio.mode(PIR, gpio.INT)
+    function build_post_request(path, value)
+        payload = cjson.encode(value)
 
-movement = 0
-
-
-local manager = dofile(SELF_REGISTRATION)(IP, PORT, REGISTRATION_ENDPOINT)
-manager.register()
-
-function build_post_request(path, value)
-    payload = cjson.encode(value)
-
-    return "POST " .. path .. " HTTP/1.1\r\n" ..
-    "Host: " .. IP .. "\r\n" ..
-    "Connection: close\r\n" ..
-    "Content-Type: application/json\r\n" ..
-    "Content-Length: " .. string.len(payload) .. "\r\n" ..
-    "\r\n" .. payload
-end
-
-function result_post(alarm)
-    
-    conn = net.createConnection(net.TCP, 0)
-    conn:connect(PORT, IP)
-
-    --- Create HTTP POST raw headers and body
-    request = build_post_request(ENDPOINT, {movement = alarm})
-
-    conn:send(request, function()
-        print("Request sent")
-        conn:close()
-    end)
-end
-
---- Get temp and send data to thingspeak.com
-local function collect_data()
-    
-    local alarm = gpio.read(PIR)
-    
-    if movement == alarm then
-        if movement == 0 then
-            gpio.write(LED, gpio.HIGH)
-        end
-    else
-        print("Movement")
-        published = true
-        movement = alarm
-        gpio.write(LED, gpio.LOW)
-        result_post(alarm)
+        return "POST " .. path .. " HTTP/1.1\r\n" ..
+        "Host: " .. IP .. "\r\n" ..
+        "Connection: close\r\n" ..
+        "Content-Type: application/json\r\n" ..
+        "Content-Length: " .. string.len(payload) .. "\r\n" ..
+        "\r\n" .. payload
     end
+
+    function result_post(payload)
+
+        conn = net.createConnection(net.TCP, 0)
+        conn:connect(PORT, IP)
+
+        --- Create HTTP POST raw headers and body
+        request = build_post_request(ENDPOINT, payload)
+
+        conn:send(request, function()
+            print("Request sent")
+            conn:close()
+        end)
+    end
+
+    --- Get temp and send data to thingspeak.com
+    local function collect_data()
+        --- Read PIR value
+        local movement = gpio.read(PIR)
+
+        --- If we have a change in state
+        if self.movement == movement then
+            --- and the current state is off
+            if self.movement == 0 then
+                --- switch LED off
+                gpio.write(LED, gpio.HIGH)
+            end
+        else
+            print("Movement")
+            --- we store our current state
+            self.movement = movement
+            --- switch LED on
+            gpio.write(LED, gpio.LOW)
+            --- POST current value
+            result_post({movement = movement})
+        end
+    end
+
+    ------ MAIN ------
+    -- send data every X ms to thing speak
+    function self.initialize()
+        print("Initializing PIR module")
+        --- set GPIO modes
+        gpio.mode(LED, gpio.OUTPUT)
+        gpio.mode(PIR, gpio.INT)
+
+        --- capture changes on PIR
+        gpio.trig(PIR, "both", collect_data)
+
+        --- check every 2 seconds the PIR status
+        tmr.alarm(2, 2000, 1, collect_data)
+    end
+
+    return self
 end
 
------- MAIN ------
--- send data every X ms to thing speak
-gpio.trig(PIR, "both", collect_data)
-
-tmr.alarm(2, 2000, 1, collect_data)
+return exports
