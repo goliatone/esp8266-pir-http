fc1fe8a0647fc37e992cd8b61dae1a9ae1b6a034
goliatone <burgosemi@gmail.com>
2015-10-17 15:39:30 -0400

    added run program

---
+++ src/run_program.lua
@@ -0,0 +1,15 @@
+-- Start your normal program routines here
+print("Execute code")
+dofile("config.lc")
+wifi.setmode(wifi.STATION)
+wifi.sta.config(ssid,password)
+wifi.sta.connect()
+
+ssid=nil
+password=nil
+
+-- check if mqtt-client.lc exists before execute
+if file.open("mqtt-client.lc") then
+     file.close("mqtt-client.lc")
+     dofile("mqtt-client.lc")
+end
