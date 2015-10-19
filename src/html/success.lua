876125dcf4fd73afc9ea5dd208c39eb238210102
goliatone <burgosemi@gmail.com>
2015-10-18 23:19:44 -0400

    html: added success

---
+++ src/html/success.lua
@@ -0,0 +1,6 @@
+local success = [[<h3>Configuration is now complete</h3>
+    <h4>The module MAC address is: ${ap_mac}</h4>
+    <h4>IP address is: ${staip}</h4>
+    <h4>Rebooting now...</h4>
+    </body> </html>]]
+return success
