87ed03fabcf3214436db6e5bddef4bdf870fc8fd
goliatone <burgosemi@gmail.com>
2015-10-18 23:19:12 -0400

    html: adding header

---
+++ src/html/header.lua
@@ -0,0 +1,11 @@
+local header = [[<!DOCTYPE html>
+<html lang='en'>
+<style>
+html { height:95%; font-family: Helvetica, Verdana,serif; color:#fafafa;background-color:#212121; margin:30px; }
+body{ width:80%; margin-left:auto; margin-right:auto; max-width:600px;}
+table,input[type=text],input[type=password] {width:100%}
+tr{line-height:30px;}
+</style>
+<body><h1>Wireless setup</h1><hr/>]]
+
+return header
