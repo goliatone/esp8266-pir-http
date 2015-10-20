945e6b3c50cff00d5000d17e640bd787aa31fe7f
goliatone <burgosemi@gmail.com>
2015-10-19 20:52:24 -0400

    ...

--- src/run_config.lua
+++ src/run_config.lua
@@ -22,7 +22,6 @@ local unescape = function (s)
 end
 
 print("Get available APs")
-local MAX_SSID_OPTIONS = 7
 wifi.setmode(wifi.STATION)
 wifi.sta.getap(function(t)
     available_aps = ""
@@ -31,10 +30,9 @@ wifi.sta.getap(function(t)
         for k,v in pairs(t) do
             ap = string.format("%-10s",k)
             ap = trim(ap)
-            print("AP: "..ap)
             available_aps = available_aps .. "<option value='".. ap .."'>".. ap .."</option>"
-            count = count + 1
-            if (count >= MAX_SSID_OPTIONS) then break end
+            count = count+1
+            if (count>=10) then break end
         end
         available_aps = available_aps .. "<option value='-1'>---hidden SSID---</option>"
         setup_server()
@@ -42,14 +40,12 @@ wifi.sta.getap(function(t)
 end)
 
 function setup_server()
+
     -- Prepare HTML form
     print("Preparing HTML Form")
-    collectgarbage()
-    if (file.open('config.html','r')) then
-    -- if (file.open('html/configform.html','r')) then
+    if (file.open('html/configform.html','r')) then
         buf = file.read()
         file.close()
-        collectgarbage()
     end
     -- interpret variables in strings
     -- ssid="WLAN01"
@@ -59,33 +55,17 @@ function setup_server()
         return _G[w:sub(3, -2)] or ""
     end)
 
-    print("Setting up Wifi AP")
+  print("Setting up Wifi AP")
     wifi.setmode(wifi.SOFTAP)
-    -- configure our connection
-    local cfg={}
-    cfg.ssid = "WEETHING_CONFIG_"..node.chipid()
-    cfg.pwd  = "weeconfig"
-    wifi.ap.config(cfg)
-    print("WE ARE CONFIGURING: "..cfg.ssid.." pwd "..cfg.pwd)
-    --- get MAC now
-    ap_mac = wifi.ap.getmac()
+  local cfg={}
+  cfg.ssid = "ESPconfig"
+  cfg.pwd  = "espconfig"
+  wifi.ap.config(cfg)
 
-    local ipcfg={}
-    ipcfg.ip = "192.168.1.1"
-    ipcfg.netmask = "255.255.255.0"
-    ipcfg.gateway = "192.168.1.1"
-
-    wifi.ap.setip(ipcfg)
-
-    print(wifi.ap.getip())
-
-    print("Setting up webserver")
-
-    local srv = nil
-    srv = net.createServer(net.TCP)
-
+  print("Setting up webserver")
+  local srv = nil
+    srv=net.createServer(net.TCP)
     srv:listen(80,function(conn)
-
         conn:on("receive", function(client,request)
             local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
             if(method == nil)then
@@ -100,30 +80,34 @@ function setup_server()
 
             if (_GET.password ~= nil and _GET.ssid ~= nil) then
                 if (_GET.ssid == "-1") then _GET.ssid=_GET.hiddenssid end
-                client:send("Saving data..");
+                print("Saving data..")
                 file.open("config.lua", "w")
-                file.writeline('ssid = "' .. _GET.ssid .. '"')
-                file.writeline('password = "' .. _GET.password .. '"')
                 -- write every variable in the form
                 for k,v in pairs(_GET) do
                     file.writeline(k..' = "'..v ..'"')
+                    -- file.writeline('ssid = "' .. _GET.ssid .. '"')
                 end
                 file.close()
                 node.compile("config.lua")
-                file.remove("config.lua")
-                buffered(buf, function() client:send() end, function()
-                    print("All stuff sent!...restarting in a bit")
-                    tmr.alarm (0, 4000,0, dorestart)
-                end)
+                -- file.remove("config.lua")
+                local header = "<!DOCTYPE html><html lang='en'><body><h1>Wireless setup</h1><br/>"
+                local success = [[<h3>Configuration is now complete</h3>
+                    <h4>The module MAC address is: ]]..wifi.ap.getmac()..[[</h4>
+                    <h4>IP address is: ]]..wifi.ap.getip()..[[</h4>
+                    <h4>Rebooting now...</h4>
+                    </body> </html>]]
+                client:send(header..success, function(client)
+                    client:close()
+                    oncomplete()
+                end);
+            else
+                payloadLen = string.len(buf)
+                client:send("HTTP/1.1 200 OK\r\n")
+                client:send("Content-Type    text/html; charset=UTF-8\r\n")
+                client:send("Content-Length:" .. tostring(payloadLen) .. "\r\n")
+                client:send("Connection:close\r\n\r\n")
+                client:send(buf, function(client) client:close() end);
             end
-
-            payloadLen = string.len(buf)
-            client:send("HTTP/1.1 200 OK\r\n")
-            client:send("Content-Type    text/html; charset=UTF-8\r\n")
-            client:send("Content-Length:" .. tostring(payloadLen) .. "\r\n")
-            client:send("Connection:close\r\n\r\n")
-            buffered(buf, function(str)client:send(str)end, function(client)client.close()end)
-            -- client:send(buf, function(client) client:close() end);
         end)
     end)
     print("Setting up Webserver done. Please connect to: " .. wifi.ap.getip())
@@ -133,19 +117,7 @@ function trim(s)
     return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-function dorestart()
-    node.restart()
-end
-
-function buffered(str, send, ondone)
-    print("buffering chunks")
-    -- 1460 is the max we can send. Frames are actually 1500, but header
-    index = 0; offset = 1400; total = string.len(str); i = 0;
-    repeat
-        chunk = string.sub(str, index, index + offset)
-        index = index + offset
-        send(chunk)
-        i = i + 1
-    until (index > total) or (i > 100)
-    ondone()
+function oncomplete()
+    -- node.restart();
+    dofile("conf_init.lua")
 end
