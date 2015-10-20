e2d9c4b8d991e7805fce90ed3f224a8000d209f2
goliatone <burgosemi@gmail.com>
2015-10-19 23:48:15 -0400

    init.lua wait two seconds

--- src/init.lua
+++ src/init.lua
@@ -1,16 +1,16 @@
--- Initialize the board but give us a few seconds to send a 
--- file.remove('init.lua') 
+-- Initialize the board but give us a few seconds to send a
+-- file.remove('init.lua')
 
 print("init.lua executing")
 
-local BOOT_DELAY = 1000
 local BOOT_ALARM = 0
+local BOOT_DELAY = 2000
 local BOOT_FILE = "boot.lua"
 
 print("starting delay to run "..BOOT_FILE)
 
 tmr.alarm(BOOT_ALARM, BOOT_DELAY, 0, function()
-    print("Stop alarm... dofile('"..BOOT_FILE.."')")    
+    print("Stop alarm... dofile('"..BOOT_FILE.."')")
     tmr.stop(BOOT_ALARM)
     dofile(BOOT_FILE)
 end)
