ec394d89d28ea87cd7e039518d04d3c16fa81757
goliatone <burgosemi@gmail.com>
2015-10-19 23:47:58 -0400

    boot.lua refactoring

--- src/boot.lua
+++ src/boot.lua
@@ -7,37 +7,39 @@ print("Setting up wifi")
 
 --- we load config file, hardcoded values in config.lua
 --- file, declared as global
-CONFIG_FILE = "config.lua"
+
 c = file.list()
-if c[CONFIG_FILE] then
-  dofile(CONFIG_FILE)
-end
+if not c["config.lc"] then
+    dofile("ap.lua")
+else
 
+    dofile("config.lc")
 
-wifi.setmode(wifi.STATION)
-wifi.sleeptype(wifi.NONE_SLEEP)
-wifi.sta.config(SSID, PSWD)
+    wifi.setmode(wifi.STATION)
+    wifi.sleeptype(wifi.NONE_SLEEP)
+    wifi.sta.config(ssid, password)
 
-wifi.sta.connect()
+    wifi.sta.connect()
 
---- Start the WiFi connection
-local count = 0
-local maxtries = 20
+    --- Start the WiFi connection
+    local count = 0
+    local maxtries = 20
 
-local WIFI_ALARM = 1
-local WIFI_DELAY = 1000
+    local WIFI_ALARM = 1
+    local WIFI_DELAY = 1000
 
-tmr.alarm(WIFI_ALARM, WIFI_DELAY, 1, function()
-    if wifi.sta.status() ~= 5 and count < maxtries then
-        print("Connecting..."..count)
-        count = count + 1
-    else
-        tmr.stop(WIFI_ALARM)
-        if count == maxtries then
-            print("Unable to connect to WiFi")
+    tmr.alarm(WIFI_ALARM, WIFI_DELAY, 1, function()
+        if wifi.sta.status() ~= 5 and count < maxtries then
+            print("Connecting..."..count)
+            count = count + 1
         else
-            print("** Done..."..wifi.sta.getip())
-            dofile("main.lua")
+            tmr.stop(WIFI_ALARM)
+            if count == maxtries then
+                print("Unable to connect to WiFi")
+            else
+                print("** Done..."..wifi.sta.getip())
+                dofile("main.lua")
+            end
         end
-    end
-end)
+    end)
+end
