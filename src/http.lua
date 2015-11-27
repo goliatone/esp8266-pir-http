e861639d7b17d03cf733568444066b6a067ee34a
goliatone <burgosemi@gmail.com>
2015-11-27 10:28:47 -0500

    adding methods to http

--- src/http.lua
+++ src/http.lua
@@ -2,32 +2,58 @@ local http = {}
 
 function http.parseUrl(url)
     local components = {}
+
     components.scheme = string.match(url, "([^:]*):")
-    components.host = string.match(url, components.scheme .. "://([^:/]*)[:/]?")
-    components.port = string.match(url, components.scheme .. "://" .. components.host .. ":([%d]*)")
-    baseUrl = components.scheme .. "://" .. components.host
+    components.host = string.match(url, components.scheme.."://([^:/]*)[:/]?")
+    components.port = string.match(url, components.scheme.."://"..components.host..":([%d]*)")
+
+    baseUrl = components.scheme.."://"..components.host
+
     if components.port ~= nil then
-        baseUrl = baseUrl .. ":" .. components.port
+        baseUrl = baseUrl..":"..components.port
     end
+
     components.pathAndQueryString = string.sub(url, string.len(baseUrl) + 1)
+
     return components
 end
 
-function http.getContent(url, callWithData)
-    http.sendContent("GET", url, nil, nil, callWithData)
+local unescape = function (s)
+    s = string.gsub(s, "+", " ")
+    s = string.gsub(s, "%%(%x%x)", function (h)
+        return string.char(tonumber(h, 16))
+    end)
+    return s
 end
 
-function http.postContent(url, content, contentType, callWithData)
-    http.sendContent("POST", url, content, contentType, callWithData)
+function http.parseQuerystring(payload)
+    local _GET = {}
+    local parameters = string.match(payload, "^GET (.*) HTTP/%d.%d")
+    if parameters ~= nil then
+        for k, v in string.match(parameters, "([_%w]+)=([^%&]+)&*") do
+            _GET[k] = unescape(v)
+        end
+    end
+    return _GET
 end
 
-function http.sendContent(method, url, contentToSend, contentType, callWithData)
+function http.get(url, callback)
+    http.sendContent("GET", url, nil, nil, callback)
+end
+
+function http.post(url, content, contentType, callback)
+    http.sendContent("POST", url, content, contentType, callback)
+end
+
+function http.sendContent(method, url, contentToSend, contentType, callback)
     local components = http.parseUrl(url)
+
     if components.port == nil then
         components.port = 80
     else
         components.port = tonumber(components.port)
     end
+
     if components.pathAndQueryString == nil or components.pathAndQueryString == "" then
         components.pathAndQueryString = "/"
     end
@@ -37,33 +63,41 @@ function http.sendContent(method, url, contentToSend, contentType, callWithData)
     end
 
     local conn=net.createConnection(net.TCP, false)
+
     conn:on("connection", function(conn)
-        conn:send(method .. " " .. components.pathAndQueryString .. " HTTP/1.0\r\nHost: " .. components.host .. "\r\n"
-            .. "Accept: */*\r\n")
+        conn:send(method.." "..components.pathAndQueryString.." HTTP/1.1\r\nHost: "..components.host.."\r\n"
+            .."Accept: */*\r\n")
 
 
         if contentToSend ~= nil then
-            conn:send("Content-Type: " .. contentType .. "\r\n");
-            conn:send("Content-Length: " .. string.len(contentToSend) .. "\r\n\r\n")
+            conn:send("Content-Type: "..contentType.."\r\n");
+            conn:send("Content-Length: "..string.len(contentToSend).."\r\n\r\n")
             conn:send(contentToSend)
         else
             conn:send("\r\n")
         end
 
     end)
-    conn:on("receive", function(conn, pl)
+
+    conn:on("receive", function(conn, payload)
         local data = {}
-        data.status = string.match(pl, "HTTP/%d.%d (%d+)")
-        local location = string.find(pl, "\r\n\r\n")
+
+        data.status = string.match(payload, "HTTP/%d.%d (%d+)")
+
+        local location = string.find(payload, "\r\n\r\n")
+
         if location ~= nil then
-            data.content = string.sub(pl, location + 4)
+            data.content = string.sub(payload, location + 4)
         end
-        pl = nil
+
+        payload = nil
+
         collectgarbage()
+
         conn:close()
         conn = nil
 
-        callWithData(data)
+        callback(data)
 
         data = nil
     end)
